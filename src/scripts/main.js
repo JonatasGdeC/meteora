06773482accb825d8ab37faf3bbef8270b7184da
Jonatas <jonatas.07022003@gmail.com>
2023-06-21 00:06:43 -0300

    Criação da seção de banner e carrousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,33 @@ menuHamburguer.click(function(){
 menuFechar.click(function(){
     $('#menu--mobile').slideToggle()
 })
+
+//Programação para Carrousel de Banners
+$('.banner__desktop').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true
+});
+
+$('.banner__tablet').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true
+});
+
+$('.banner__mobile').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true
+});
+
+
