32ac56dddacecc6b6b68de279cd1d28fa1ed4614
Jonatas <jonatas.07022003@gmail.com>
2023-06-20 22:54:11 -0300

    Criação do header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('testando!')
+//Programação para menu hamburguer
+const menuHamburguer = $('#menu--burguer')
+const listaMenuMobile = $('#menu--mobile')
+const menuFechar = $('#menu--close')
+
+menuHamburguer.click(function(){
+    $('#menu--mobile').slideToggle()
+})
+menuFechar.click(function(){
+    $('#menu--mobile').slideToggle()
+})
