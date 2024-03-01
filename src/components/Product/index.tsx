4b4a3521fcb86a03c5f7cac967b2deb3e1a49618
Jonatas <jonatas.07022003@gmail.com>
2024-03-01 01:25:41 -0300

    Criando o modal

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,4 +1,6 @@
 import { Button } from '../../styles'
+import iconCheck from '../../assets/icons/check.png'
+import iconClose from '../../assets/icons/close.png'
 import * as S from './styles'
 
 type Props = {
@@ -17,15 +19,84 @@ export const parseToBrl = (amount = 0) => {
 
 const Product = ({ image, title, description, price }: Props) => {
   return (
-    <S.Card>
-      <img src={image} alt={title} />
-      <S.Details>
-        <h3>{title}</h3>
-        <p className="description">{description}</p>
-        <p className="price">{parseToBrl(price)}</p>
-        <Button>Ver mais</Button>
-      </S.Details>
-    </S.Card>
+    <>
+      <S.Card>
+        <img src={image} alt={title} />
+        <S.Details>
+          <h3>{title}</h3>
+          <p className="description">{description}</p>
+          <p className="price">{parseToBrl(price)}</p>
+          <Button>Ver mais</Button>
+        </S.Details>
+      </S.Card>
+      <S.Modal>
+        <S.Overlay />
+        <S.CardModal>
+          <S.InfosModal>
+            <img src="https://fakeimg.pl/350x422" alt="" />
+            <div>
+              <h3>Título</h3>
+              <p className="description">Descrição</p>
+              <p className="price">Preço</p>
+              <p className="sale">Vendido e entregue por Riachuelo</p>
+              <form>
+                <div className="section">
+                  <p>Cores:</p>
+                  <div className="options">
+                    <div>
+                      <label htmlFor="azul">Azul claro</label>
+                      <input name="color" type="radio" id="azul" />
+                    </div>
+                    <div>
+                      <label htmlFor="branco">Offwhite</label>
+                      <input name="color" type="radio" id="branco" />
+                    </div>
+                    <div>
+                      <label htmlFor="preto">Preto</label>
+                      <input name="color" type="radio" id="preto" />
+                    </div>
+                  </div>
+                </div>
+                <div className="section">
+                  <p>Tamanho:</p>
+                  <div className="options">
+                    <div>
+                      <label htmlFor="p">P</label>
+                      <input name="size" type="radio" id="p" />
+                    </div>
+                    <div>
+                      <label htmlFor="pp">PP</label>
+                      <input name="size" type="radio" id="pp" />
+                    </div>
+                    <div>
+                      <label htmlFor="mm">M</label>
+                      <input name="size" type="radio" id="m" />
+                    </div>
+                    <div>
+                      <label htmlFor="g">G</label>
+                      <input name="size" type="radio" id="g" />
+                    </div>
+                    <div>
+                      <label htmlFor="gg">GG</label>
+                      <input name="size" type="radio" id="gg" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </S.InfosModal>
+          <S.FooterModal>
+            <div>
+              <img src={iconCheck} alt="Check" />
+              <p>Confira detalhes sobre o produto</p>
+            </div>
+            <button>
+              <img src={iconClose} alt="Close" />
+            </button>
+          </S.FooterModal>
+        </S.CardModal>
+      </S.Modal>
+    </>
   )
 }
 
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -4,8 +4,13 @@ import { colors } from '../../styles'
 export const Card = styled.li`
   max-width: 350px;
   border: 1px solid ${colors.gray};
+  transition: all ease 0.3s;
   img {
     width: 100%;
+  }
+
+  &:hover {
+    scale: 1.03;
   }
 `
 export const Details = styled.div`
@@ -26,5 +31,115 @@ export const Details = styled.div`
     font-size: 16px;
     font-weight: bold;
     margin-bottom: 10px;
+  }
+`
+export const Modal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+`
+export const CardModal = styled.div`
+  background-color: ${colors.white};
+`
+
+export const InfosModal = styled.div`
+  padding: 16px 16px 32px;
+  display: flex;
+  gap: 16px;
+  align-items: top;
+
+  div {
+    h3 {
+      font-size: 16px;
+      font-weight: bold;
+      padding-bottom: 24px;
+    }
+
+    .description {
+      font-size: 13px;
+      line-height: 20px;
+      padding-bottom: 24px;
+      border-bottom: 1px solid ${colors.black};
+    }
+
+    .price {
+      font-size: 20px;
+      font-weight: 500;
+      padding: 24px 0;
+    }
+
+    .sale {
+      font-size: 13px;
+      color: ${colors.gray};
+      padding-bottom: 16px;
+    }
+
+    form {
+      .section {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        padding: 16px 0;
+        border-top: 1px solid ${colors.gray};
+
+        p {
+          font-size: 13px;
+          font-weight: bold;
+        }
+
+        .options {
+          display: flex;
+          gap: 16px;
+
+          div {
+            display: flex;
+            flex-direction: column-reverse;
+            gap: 8px;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+`
+
+export const FooterModal = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 16px;
+  background-color: ${colors.black};
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+
+    p {
+      font-size: 20px;
+      color: ${colors.white};
+    }
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
   }
 `
