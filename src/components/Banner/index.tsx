35ad02dfbe7a6bd553ddbda32e534611b1e7690f
Jonatas <jonatas.07022003@gmail.com>
2024-03-03 13:51:49 -0300

    Atualizando icon

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,17 +2,17 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-import imageBannerDesktop1 from '../../assets/Desktop/bannerCarousel1.png'
-import imageBannerDesktop2 from '../../assets/Desktop/bannerCarousel2.png'
-import imageBannerDesktop3 from '../../assets/Desktop/bannerCarousel3.png'
+import imageBannerDesktop1 from '../../assets/banner/desktop/bannerCarousel1.png'
+import imageBannerDesktop2 from '../../assets/banner/desktop/bannerCarousel2.png'
+import imageBannerDesktop3 from '../../assets/banner/desktop/bannerCarousel3.png'
 
-import imageBannerTablet1 from '../../assets/Tablet/bannerCarousel1.png'
-import imageBannerTablet2 from '../../assets/Tablet/bannerCarousel2.png'
-import imageBannerTablet3 from '../../assets/Tablet/bannerCarousel3.png'
+import imageBannerTablet1 from '../../assets/banner/tablet/bannerCarousel1.png'
+import imageBannerTablet2 from '../../assets/banner/tablet/bannerCarousel2.png'
+import imageBannerTablet3 from '../../assets/banner/tablet/bannerCarousel3.png'
 
-import imageBannerMobile1 from '../../assets/Mobile/bannerCarousel1.png'
-import imageBannerMobile2 from '../../assets/Mobile/bannerCarousel2.png'
-import imageBannerMobile3 from '../../assets/Mobile/bannerCarousel3.png'
+import imageBannerMobile1 from '../../assets/banner/mobile/bannerCarousel1.png'
+import imageBannerMobile2 from '../../assets/banner/mobile/bannerCarousel2.png'
+import imageBannerMobile3 from '../../assets/banner/mobile/bannerCarousel3.png'
 
 import { Img } from './styles'
 
